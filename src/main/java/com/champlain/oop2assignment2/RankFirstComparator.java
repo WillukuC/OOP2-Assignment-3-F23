c9543ff01df8f967c98efc5c9c7a3842db27b7f5
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2023-10-01 12:55:52 -0400

    Adding the code for the RankFirstComparator class

--- src/main/java/com/champlain/oop2assignment2/RankFirstComparator.java
+++ src/main/java/com/champlain/oop2assignment2/RankFirstComparator.java
@@ -1,4 +1,16 @@
-package com.champlain.oop2assignment2;
+import java.util.Comparator;
 
-public class RankFirstComparator {
+public class RankFirstComparator implements Comparator<Card> {
+    @Override
+    public int compare(Card card1, Card card2) {
+        // Compare cards based on rank first
+        int rankComparison = card1.getRank().compareTo(card2.getRank());
+
+        // If ranks are the same, compare based on suit
+        if (rankComparison == 0) {
+            return card1.getSuit().compareTo(card2.getSuit());
+        }
+
+        return rankComparison;
+    }
 }
